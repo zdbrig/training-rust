22b033ad1e283b2926662d153b5a51a89794fc09
karima <zouaoui>
2021-09-13 09:19:52 +0100

    first items

--- sqoinrust/src/main.rs
+++ sqoinrust/src/main.rs
@@ -1,3 +1,23 @@
+
+
+pub struct Sqoiner {
+    number: i32,
+}
+
+
+pub trait SqoinRules {
+    fn shownumber(sqoiner: & Sqoiner) ; 
+}
+
+
+pub fn print_sqoiner(sqoiner: &Sqoiner) -> ()
+ {
+     println!(" Printing Sqoin {}" , sqoiner.number);
+ }
+
+
 fn main() {
-    println!("Hello, world!");
+    
+    print_sqoiner(&Sqoiner{number: 10});
+
 }
