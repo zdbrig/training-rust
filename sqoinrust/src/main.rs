e6e7e69eb012dc72a77165530087c0670b674ee4
karima <zouaoui>
2021-09-13 14:22:13 +0100

    Add struct and impl

--- sqoinrust/src/main.rs
+++ sqoinrust/src/main.rs
@@ -23,7 +23,7 @@ mod karima {
 
 mod jawaher;
 mod khouloud;
-
+use std::fmt::{Display, Formatter, Result};
 fn main() {
     println!("------- Modules ----------");
 
@@ -168,10 +168,24 @@ fn main() {
 
     println!("--------- Functions --------");
 
-    println!(" sum of integers : {}" , sum::<u32>(10u32 , 20u32));
-    println!(" sum of integers : {}" , sum(1.2f64 , 1.3f64));
+    println!(" sum of integers : {}", sum::<u32>(10u32, 20u32));
+    println!(" sum of integers : {}", sum(1.2f64, 1.3f64));
 
     //   println!("{}" , trainee);
+
+    println!(" ------------ Structs ------------- ");
+
+    let mut p: PorfolioAccount = PorfolioAccount::new("bacem porfolio ", 0.0, 0.0);
+
+    p.deposit_bitcoin(2.3);
+
+    p.deposit_bitcoin(3.3);
+
+    println!(" porfolio de Bacem est : {}", p);
+
+    let p: PorfolioAccount = PorfolioAccount::new("Amal porfolio ", 6.1, 8.3);
+
+    println!(" porfolio de Bacem est : {}", p);
 }
 
 pub enum Trainee {
@@ -181,11 +195,9 @@ pub enum Trainee {
 }
 use std::ops::Add;
 
-
-fn sum<T : Add<Output = T>>(x1:  T, x2:  T) -> T {
-
+fn sum<T: Add<Output = T>>(x1: T, x2: T) -> T {
     let sum = x1 + x2;
-    sum 
+    sum
 }
 
 fn print_box(b: Box<u32>) -> Box<u32> {
@@ -196,5 +208,41 @@ fn print_box(b: Box<u32>) -> Box<u32> {
 fn print_slice(v: &[u32]) {
     for i in v {
         print!("{} ", i);
+    }
+}
+
+enum Currency {
+    BITCOIN,
+    ETHEREUM,
+}
+
+struct PorfolioAccount {
+    name: String,
+    values: [(Currency, f64); 2],
+}
+
+impl Display for PorfolioAccount {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        writeln!(
+            f,
+            " Porfolio  : name = {} , BITCOIN = {} , ETH = {}",
+            self.name, self.values[0].1, self.values[1].1
+        );
+        Result::Ok(())
+    }
+}
+
+impl PorfolioAccount {
+    pub fn new(name: &str, bitcoin: f64, eth: f64) -> PorfolioAccount {
+        PorfolioAccount {
+            name: name.to_string(),
+            values: [(Currency::BITCOIN, bitcoin), (Currency::ETHEREUM, eth)],
+        }
+    }
+
+    pub fn deposit_bitcoin(&mut self , d: f64) {
+        let mut old = self.values[0].1;
+        old += d;
+        self.values[0] = (Currency::BITCOIN, old);
     }
 }
