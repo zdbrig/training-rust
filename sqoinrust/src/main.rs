41367bc8d1ae6bbc740db5140ef8926d6afe4875
karima <zouaoui>
2021-09-13 10:13:23 +0100

    add variable definitions

--- sqoinrust/src/main.rs
+++ sqoinrust/src/main.rs
@@ -38,5 +38,31 @@ fn main() {
  
     karima::say_hello();
     khouloud::say_hello();
-    jawaher::jawaherimpl::say_hello();
+    jawaher::jawaherimpl::say_hello(); 
+
+
+    let x = 100u32 + ( 10u8 as u32);
+
+     let y:u32 =  1000u32;
+
+     let f:f32 = 0.001f32;
+
+    let b: bool =  {
+            let mut ret = false;
+            if x < y {
+                ret = true;
+            }
+            ret
+      };
+
+    let func = move |z:u32| {
+        let mut s = x;
+        s = s * z *z;
+        s
+    };
+    println!(" value of x is {}" , x);
+    println!(" value of y is {}" , y);
+    println!(" value of f is {}" , f);
+    println!(" value of b is {}" , b);
+    println!(" value of func(y) is {}" , func(y));
 }
