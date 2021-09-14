2d897a8644e3231751aa4c9700eda0dd6e0e1246
karima <zouaoui>
2021-09-14 12:07:50 +0100

    iter adapters part 4

--- lesson2/src/main.rs
+++ lesson2/src/main.rs
@@ -224,4 +224,36 @@ fn main() {
         }
     }
 
+    println!("************ consuming iterators *************");
+
+    let numbers:Vec<u32> = vec![ 1 , 2 , 5 , 7 , 10];
+
+    let  numbes_iter = (&numbers).into_iter();
+    
+    println!(" sum = {}" , numbes_iter.sum::<u32>() );
+
+    let  numbes_iter = (&numbers).into_iter();
+
+    println!(" product = {}" , numbes_iter.product::<u32>() );
+
+    let  numbes_iter = (&numbers).into_iter();
+
+    println!(" count = {}" , numbes_iter.count() );
+
+    let  numbes_iter = (&numbers).into_iter();
+
+    println!(" min = {}" , numbes_iter.min().unwrap() );
+
+    let mut numbes_iter = (&numbers).into_iter();
+
+    println!(" any  > 8 = {}" , numbes_iter.any(|m| m > &8u32 ) );
+
+    let mut numbes_iter = (&numbers).into_iter();
+
+    println!(" all  > 8 = {}" , numbes_iter.all(|m| m > &8u32 ) );
+
+    let mut numbes_iter = (&numbers).into_iter();
+
+    println!(" position  de  7 = {}" , numbes_iter.position(|m| m > &5000u32 ).unwrap_or(0) );
+
 }
