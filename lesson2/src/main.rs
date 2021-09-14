03440e3b6188ca95c476da064f991a6be7dcffd2
karima <zouaoui>
2021-09-14 10:13:32 +0100

    iter adapters part 1

--- lesson2/src/main.rs
+++ lesson2/src/main.rs
@@ -1,6 +1,7 @@
-
-pub trait Wallet<T,U> 
-where T: Clone + Sized + Copy , U: Clone
+pub trait Wallet<T, U>
+where
+    T: Clone + Sized + Copy,
+    U: Clone,
 {
     type ReturnType;
     type ReturnType2;
@@ -8,36 +9,87 @@ where T: Clone + Sized + Copy , U: Clone
     fn show_wallet(&self) -> Self::ReturnType;
 }
 
-impl  Wallet<u32,u32> for u32 {
-    type ReturnType =  u64;
+impl Wallet<u32, u32> for u32 {
+    type ReturnType = u64;
     type ReturnType2 = u64;
-     fn show_wallet(&self) -> u64 {
-         10u64
+    fn show_wallet(&self) -> u64 {
+        10u64
     }
-
 }
 
 fn main() {
-   
-    let rust_group = vec![ "Bacem" , "Karima" , "Amal" , "Jawaher" , "Halima" , "Khouloud"];
-    
-   /* for member in &rust_group {
+    let rust_group = vec!["Bacem", "Karima", "Amal", "Jawaher", "Halima", "Khouloud"];
+
+    /* for member in &rust_group {
         println!("{}" ,  member);
     }*/
 
     let mut iter = (&rust_group).into_iter();
-    
     let value = iter.next();
     let real_value = match value {
         Some(v) => v,
-        None => ""
+        None => "",
     };
 
-    println!("{}" , real_value);
-
+    println!("{}", real_value);
 
     while let Some(v) = iter.next() {
-        println!("{}" , v);
+        println!("{}", v);
     }
-    
+
+    println!("************ map *************");
+
+    let y = rust_group.iter().map(|m| (String::new() + m + " kind"));
+    for member in y {
+        println!("{}", member);
+    }
+
+    println!("************ filter *************");
+
+    let y = rust_group.iter().filter(|m| m.len() <= 5);
+
+    for member in y {
+        println!("{}", member);
+    }
+    println!("************ filter map *************");
+
+    let z = rust_group.iter().filter_map(|m| {
+        if m.len() > 5 {
+            return Some(String::new() + m + " kind ");
+        }
+        None
+    });
+    for m1 in z {
+        println!("{}", m1);
+    }
+    println!("************ flat map *************");
+
+    let z = rust_group.iter().flat_map(|m| {
+        if m.len() > 5 {
+            return Some([m, " kind "]);
+        }
+        None
+    });
+    for member in z {
+        print!("[");
+        for m1 in member {
+            print!("{} ", m1);
+        }
+        print!("]");
+    }
+
+    //let it: Vec<u32> = (1..25).collect();
+
+    println!("************ scan *************");
+
+    let iter = rust_group.iter().scan(" Sqoin members are ".to_string(),
+        | s , item| {
+            s.push_str(item);
+            s.push_str(" ");
+             Some(s.clone())
+        }
+    );
+    for m1 in iter {
+        println!("{}", m1);
+    }
 }
