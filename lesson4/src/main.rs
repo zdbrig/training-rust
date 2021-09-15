00a01561f9205468f0b0d949def039124fa8b3f5
karima <zouaoui>
2021-09-15 10:11:45 +0100

    formatting number

--- lesson4/src/main.rs
+++ lesson4/src/main.rs
@@ -31,20 +31,108 @@ fn main() {
 
         let mut sname = String::new();
         sname.push_str("Bergaoui");
-        
-        let name_to_string:String = name.to_string();
 
-        let charachter: [char; 4] = ['A' , 'm' , 'a' , 'l'];
+        let name_to_string: String = name.to_string();
+
+        let charachter: [char; 4] = ['A', 'm', 'a', 'l'];
         use std::slice::Iter;
-        let iter: Iter::<char>= charachter.iter();
+        let iter: Iter<char> = charachter.iter();
 
         let collection: String = iter.collect();
 
-        println!(" {} " , collection);
+        println!(" {} ", collection);
 
-        println!("len = {}" , name.len());
+        println!("len = {}", name.len());
 
-        println!("is_empty = {}" , name.is_empty());
+        println!("is_empty = {}", name.is_empty());
+
+        println!("range = {}", &name[1..4]);
+
+        println!("sname = {}", &sname);
+
+        sname.push('1');
+
+        println!("sname = {}", &sname);
+
+        sname.extend(name.chars());
+
+        println!("sname = {}", &sname);
+
+        sname.insert(8, '✅');
+
+        println!("sname = {}", &sname);
+
+        sname.insert_str(8, "😁");
+        println!("sname = {}", &sname);
+
+        sname.clear();
+        println!("sname = {}", &sname);
+
+        sname.push_str("Jawaher Korbosli");
+        sname.truncate(8);
+        println!("sname = {}", &sname);
+
+        sname.pop();
+        sname.pop();
+        println!("sname = {}", &sname);
+        sname.remove(2);
+        println!("sname = {}", &sname);
+        sname.clear();
+        sname.push_str("Jawaher Korbosli");
+        sname.drain(4..8);
+        println!("sname = {}", &sname);
+
+        sname.clear();
+        sname.push_str("Khouloud Hadj Taieb");
+
+        let f = sname.find("dj");
+        println!("found at = {}", f.unwrap_or(0));
+        println!("contains  {}", sname.contains("mloud"));
+
+        let imen = sname.replace("Khouloud", "Imen");
+        println!("sname = {}", &imen);
+
+        let name = "Karima Zouaoui";
+
+        for i in name.chars() {
+            print!(" {} ", i);
+        }
+        println!();
+        for (i, j) in name.char_indices() {
+            print!(" [{},{}] ", i, j);
+        }
+        println!();
+        let name = "Karima 🚗 Zouaoui";
+        for i in name.bytes() {
+            print!(" {} ", i);
+        }
+        let name = "   Karima      Zouaoui \n Amal         Fathallah";
+        println!();
+        for i in name.lines() {
+            println!(" {} ", i);
+        }
+
+        let v: Vec<&str> = name.lines().collect();
+
+        let newname = name.trim();
+        println!("newname = {}", &newname);
+
+        use std::str::FromStr;
+
+        let b: bool = bool::from_str("true").unwrap_or(false);
+
+        let f: f64 = f64::from_str("12.2234244").unwrap_or(1.2);
+
+        let i = 123i32;
+
+        let v = vec![1 , 2 , 4 , 5];
+        println!("i = {1} f={0:#^50} " , f , i);
+
+        println!("v = {:?} " , v );
+
+        println!("value = {} , pointer = {:p} " , &sname , &sname );
+        let sname2 = sname.clone();
+        println!("value = {} , pointer = {:p} " , &sname2 , &sname2 );
 
 
     }
