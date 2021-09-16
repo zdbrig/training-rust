f785213a24ad5ce7a4629cc01713f69f3a6d1dbe
karima <zouaoui>
2021-09-16 11:13:20 +0100

    add vm implementation

--- lesson7/src/main.rs
+++ lesson7/src/main.rs
@@ -1,3 +1,35 @@
+use crate::virtual_machine::VirtualMachine;
+
+mod data_center;
+pub mod virtual_machine;
+pub mod vm_trait;
+pub mod vm_trait_controller;
+pub mod linux;
+pub mod macos;
+pub mod datacenterimpl;
+mod vm_type;
+
 fn main() {
-    println!("Hello, world!");
+
+    use  vm_trait::VmTrait;
+    use linux::Linux;
+    use macos::MACOS;
+    let mut o:VirtualMachine::<MACOS> =   VmTrait::<MACOS>::new();
+    
+    println!("{}" , o.info());
+    o.start();
+    println!("{}" , o.info());
+    o.pause();
+    println!("{}" , o.info());
+
+    let mut o:VirtualMachine::<Linux> =   VmTrait::<Linux>::new();
+    
+    println!("{}" , o.info());
+    o.start();
+    println!("{}" , o.info());
+    o.pause();
+    println!("{}" , o.info());
+
+
+
 }
